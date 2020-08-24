414c2be2953bdc865596e1618718cafb7dd997e6
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-08-24 17:20:39 +0700

    Remove body-parser and use express.json() instead

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const bodyParser = require("body-parser");
 
 mongoose
   .connect("mongodb://localhost:27017/acmedb", { useNewUrlParser: true })
   .then(() => {
     const app = express();
-    app.use(bodyParser.json());
-
+    app.use(express.json());
     app.use("/api", routes);
 
     app.listen(5000, () => {
